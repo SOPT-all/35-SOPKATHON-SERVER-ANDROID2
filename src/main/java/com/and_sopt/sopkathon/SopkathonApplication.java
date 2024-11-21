d2892c76ec1236df878cff626475f338e7a9b52d
jjuny-won <jjuny040627@gamil.com>
2024-11-21 19:44:54 +0900

    fix:EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/and_sopt/sopkathon/SopkathonApplication.java
+++ src/main/java/com/and_sopt/sopkathon/SopkathonApplication.java
@@ -3,6 +3,7 @@ package com.and_sopt.sopkathon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SopkathonApplication {
 
