b2fd4dffbe5f83e3c8e9bc2257b295a30f013cef
jjuny-won <jjuny040627@gamil.com>
2024-11-22 21:39:50 +0900

    chore:ci/cd postTest

--- src/main/java/com/and_sopt/sopkathon/api/controller/testController.java
+++ src/main/java/com/and_sopt/sopkathon/api/controller/testController.java
@@ -2,6 +2,8 @@ package com.and_sopt.sopkathon.api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,11 @@ public class testController {
     @GetMapping("/test") // HTTP GET 요청 처리
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("안녕하세요");
+    }
+
+
+    @PostMapping("/test/{id}") // HTTP GET 요청 처리
+    public ResponseEntity<String> postTest(@PathVariable Long id) {
+        return ResponseEntity.ok("안녕하세요 : "+id);
     }
 }
