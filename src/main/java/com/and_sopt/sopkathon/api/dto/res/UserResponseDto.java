af12a9206ea0f0057fe2161c2fe259771dbf60fb
yeeun0702 <kimyeeun0702@naver.com>
2024-11-24 01:43:08 +0900

    refactor: 명세서에 맞춰서 성공응답 부분 수정

--- src/main/java/com/and_sopt/sopkathon/api/dto/res/UserResponseDto.java
+++ src/main/java/com/and_sopt/sopkathon/api/dto/res/UserResponseDto.java
@@ -3,14 +3,12 @@ package com.and_sopt.sopkathon.api.dto.res;
 import com.and_sopt.sopkathon.api.entity.User;
 
 public record UserResponseDto(
-        long id,
-        String nickname,
+        String nickName,
         int level
 ) {
 
     public static UserResponseDto of (final User user){
         return new UserResponseDto(
-                user.getUserId(),
                 user.getNickName(),
                 user.getLevel());
     }
