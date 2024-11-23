abc7a2acbfa58cfcc6480e1575c8786337c6a2bf
jjuny-won <jjuny040627@gamil.com>
2024-11-24 02:07:37 +0900

    fix:Board 상속 수정

--- src/main/java/com/and_sopt/sopkathon/api/controller/BoardController.java
+++ src/main/java/com/and_sopt/sopkathon/api/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.and_sopt.sopkathon.api.controller;
 
-import com.and_sopt.sopkathon.api.dto.req.CreateReq;
+
+import com.and_sopt.sopkathon.api.dto.req.BoardCreateReq;
 import com.and_sopt.sopkathon.api.service.BoardService;
 import com.and_sopt.sopkathon.global.exception.CustomException;
 import com.and_sopt.sopkathon.global.response.ApiResponseDto;
@@ -19,7 +20,7 @@ public class BoardController {
     }
 
     @PostMapping("/{userId}")
-    private ApiResponseDto<?> createBoard(@PathVariable final long userId, @RequestBody CreateReq request) {
+    private ApiResponseDto<?> createBoard(@PathVariable final long userId, @RequestBody BoardCreateReq request) {
 
         if (request.getContent() == null || request.getContent().trim().isEmpty()) {
             throw new CustomException(ErrorCode.BOARD_BAD_REQUEST);
