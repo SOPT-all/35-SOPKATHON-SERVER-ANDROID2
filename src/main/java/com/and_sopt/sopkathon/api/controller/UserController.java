10e869c0988da7e249e042decec06338cc41edc5
yeeun0702 <kimyeeun0702@naver.com>
2024-11-24 03:00:04 +0900

    feat: 유저 리스트 조회

--- src/main/java/com/and_sopt/sopkathon/api/controller/UserController.java
+++ src/main/java/com/and_sopt/sopkathon/api/controller/UserController.java
@@ -2,6 +2,7 @@ package com.and_sopt.sopkathon.api.controller;
 
 
 import com.and_sopt.sopkathon.api.dto.res.UserResponseDto;
+import com.and_sopt.sopkathon.api.dto.res.UsersListResponseDto;
 import com.and_sopt.sopkathon.api.service.UserService;
 import com.and_sopt.sopkathon.global.response.ApiResponseDto;
 import com.and_sopt.sopkathon.global.response.enums.SuccessCode;
@@ -12,16 +13,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/{userId}")
+    @GetMapping("/user/{userId}")
     public ResponseEntity<ApiResponseDto<UserResponseDto>> getUserById(@PathVariable final long userId) {
         UserResponseDto userResponse = userService.findUserById(userId);
         return ResponseEntity.ok(ApiResponseDto.success(SuccessCode.USER_FOUND, userResponse));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<ApiResponseDto<List<UsersListResponseDto>>> getUsers() { // 반환 타입 명확화
+        List<UsersListResponseDto> userResponses = userService.getAllUsers();
+        return ResponseEntity.ok(ApiResponseDto.success(SuccessCode.USER_LIST_FOUND, userResponses));
     }
 }
--- src/main/java/com/and_sopt/sopkathon/global/response/enums/SuccessCode.java
+++ src/main/java/com/and_sopt/sopkathon/global/response/enums/SuccessCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
 
     //200 OK
-    USER_FOUND(20002, HttpStatus.OK, "유저를 성공적으로 찾았습니다."),
+    USER_FOUND(200, HttpStatus.OK, "유저를 성공적으로 찾았습니다."),
+    USER_LIST_FOUND(200, HttpStatus.OK, "유저 목록을 성공적으로 찾았습니다."),
     BOARD_POST_SUCCESS(20001, HttpStatus.OK, "게시글이 성공적으로 생성되었습니다.");
 
     private final int code;
