5113775a2372e3664b946fc04098c5ceae1635f5
jjuny-won <jjuny040627@gamil.com>
2024-11-24 03:22:39 +0900

    fix:Response 수정

--- src/main/java/com/and_sopt/sopkathon/global/response/enums/SuccessCode.java
+++ src/main/java/com/and_sopt/sopkathon/global/response/enums/SuccessCode.java
@@ -8,11 +8,14 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum SuccessCode {
 
-
+    //200 OK
     USER_FOUND(200, HttpStatus.OK, "유저를 성공적으로 찾았습니다."),
     USER_LIST_FOUND(200, HttpStatus.OK, "유저 목록을 성공적으로 찾았습니다."),
     BOARD_POST_SUCCESS(20001, HttpStatus.OK, "게시글이 성공적으로 생성되었습니다."),
     BOARD_LIST_FOUND(200, HttpStatus.OK, "게시글 목록을 성공적으로 찾았습니다");
+    BOARD_POST_SUCCESS(200, HttpStatus.OK, "게시글이 성공적으로 생성되었습니다."),
+    BOARD_GET_SUCCESS(200, HttpStatus.OK, "게시글이 성공적으로 조회되었습니");
+
 
     private final int code;
     private final HttpStatus httpStatus;
